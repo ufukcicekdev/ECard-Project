ce04d14dbf31c8354346c6b590c93d2fcace80d4
EAGLEEARTH <ufukcicek199@gmail.com>
2026-01-03 16:29:29 +0300

    push

--- ecard_project/urls.py
+++ ecard_project/urls.py
@@ -27,9 +27,9 @@ urlpatterns = [
 # Serve media files during develop
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 # Custom error handlers
